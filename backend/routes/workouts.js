4cae4dea30e0f9ac63c53982bc93ae36004b5968
Christian Felipe <chrisfelipe525@gmail.com>
2024-12-31 19:58:29 +0800

    updated update and delete controller for workout

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const {
     createWorkout, 
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 // GET all workouts
@@ -17,14 +19,10 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg:'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg:'UPDATE a new workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
 
