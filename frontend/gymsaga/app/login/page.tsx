61e8d18eeea6c5f0def066c07579b0ea22e20f27
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-01-03 13:47:11 +0800

    fix: move log in component to dedicated login page

---
+++ frontend/gymsaga/app/login/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { LoginForm } from "../../components/ui/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="w-screen h-screen items-center flex flex-col bg-background p-8">
+      <Image
+      className="mb-10"
+        src="/GymSaga.svg"
+        width={200}
+        height={200}
+        alt="GymSaga Logo"/>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8">Welcome to GymSaga</h1>
+      <LoginForm />
+    </div>
+  );
+}
