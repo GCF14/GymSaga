4cbc0429e57c8eb3d790eec3d19a7291c94520ec
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-01-31 15:43:32 +0800

    feat: implement create post functionality with drop zone and textarea components

--- frontend/gymsaga/components/post-button.tsx
+++ frontend/gymsaga/components/post-button.tsx
@@ -1,15 +1,28 @@
 import Link from 'next/link';
 import { Button } from './ui/button';
+import CreatePostCard from './createPost-card';
+import React from 'react';
 
 export default function PostButton() {
+  const [showCreatePostCard, setShowCreatePostCard] = React.useState(false);
+
+  const handleCreatePost = () => {
+    setShowCreatePostCard(true);
+  }
+
+  const handleCloseCreatePostCard = () => {
+    setShowCreatePostCard(false);
+  };
+
   return (
-    <Button className="mr-4">
-        <span className="material-symbols-rounded bold">
-          add
+    <>
+      <Button className="mr-4" onClick={handleCreatePost}>
+        <span className="material-symbols-rounded">
+          post_add
         </span>
-        <strong>
-            Post
-        </strong>
-    </Button>
+        Post
+      </Button>
+      {showCreatePostCard && <CreatePostCard onClose={handleCloseCreatePostCard} />}
+    </>
   );
 }
