fd00bc76a1349e6eef125df32b70825ff11e155c
Christian Felipe <chrisfelipe525@gmail.com>
2025-02-01 15:58:33 +0800

    feat: add token for signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+
+}
 // login the user
 const loginUser = async (req, res) => {
     res.json({mssg: 'login user'})
@@ -14,7 +19,10 @@ const signUpUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
         
-        res.status(200).json({email, user})
+        // create the token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
