9c7d31e48e4b033f72ede6bf1b12d2b09478a346
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-02-15 14:41:44 +0800

    feat(sheet-menu): enhance SheetMenu with responsive menu items and footer components

--- frontend/components/sheet-menu.tsx
+++ frontend/components/sheet-menu.tsx
@@ -2,14 +2,26 @@ import {
     Sheet,
     SheetContent,
     SheetDescription,
+    SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import CreatePostDialog from "./create-post-dialog"
+import { ModeToggle } from "@/components/mode-toggle"
+import SettingsButton from "@/components/settings-button"
 
 export default function SheetMenu() {
+    const menuItems = [
+        {icon: "post_add", label: "Posts", href: "/posts"},
+        {icon: "home", label: "Home", href: "/"},
+        {icon: "map", label: "Map", href: "/map"},
+        {icon: "group", label: "Social Link", href: "/social-link"},
+        {icon: "person", label: "Profile", href: "/profile"}
+    ]
+
     return (
         <Sheet>
             <SheetTrigger asChild>
@@ -19,52 +31,25 @@ export default function SheetMenu() {
                     </span>
                 </Button>
             </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle>Menu</SheetTitle>
+            <SheetContent className="flex flex-col gap-2">
+                <SheetHeader className="border-b p-4">
+                    <SheetTitle className="text-2xl">Menu</SheetTitle>
                 </SheetHeader>
-                <div className="flex flex-col mt-2 gap-2">
-                    <Link href="/posts" passHref>
-                        <Button className="w-full justify-start" variant="ghost">
-                            <span className="material-symbols-rounded">
-                                post_add
-                            </span>
-                            Posts
-                        </Button>
-                    </Link>
-                    <Link href="/" passHref>
-                        <Button className="w-full justify-start" variant="ghost">
-                            <span className="material-symbols-rounded">
-                                home
-                            </span>
-                            Home
-                        </Button>
-                    </Link>
-                    <Link href="/map" passHref>
-                        <Button className="w-full justify-start" variant="ghost">
-                            <span className="material-symbols-rounded">
-                                map
-                            </span>
-                            Map
-                        </Button>
-                    </Link>
-                    <Link href="/social-link" passHref>
-                        <Button className="w-full justify-start" variant="ghost">
-                            <span className="material-symbols-rounded">
-                                group
-                            </span>
-                            Social Link
-                        </Button>
-                    </Link>
-                    <Link href="/profile" passHref>
-                        <Button className="w-full justify-start" variant="ghost">
-                            <span className="material-symbols-rounded">
-                                person
-                            </span>
-                            Profile
-                        </Button>
-                    </Link>
+                <div className="flex flex-col gap-4 grow mt-4">
+                    {menuItems.map((item, index) => (
+                        <Link key={index} href={item.href}>
+                            <Button variant="ghost">
+                                <span className="material-symbols-rounded">{item.icon}</span>
+                                <span className="ml-2 text-xl">{item.label}</span>
+                            </Button>
+                        </Link>
+                    ))}
                 </div>
+                <SheetFooter className="flex flex-row gap-2 p-4 justify-start border-t">
+                    <CreatePostDialog />
+                    <SettingsButton />
+                    <ModeToggle />
+                </SheetFooter>
             </SheetContent>
         </Sheet>
     )
