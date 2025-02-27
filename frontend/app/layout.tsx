dbe090bb39c74d2cd5437976a67a5ece65310baf
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-02-27 19:03:09 +0800

    style: refactor CSS styles for material symbols and hover effects

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -44,62 +44,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preload" href="https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css" as="style"/>
         <link rel="preload" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" as="style"/>
         <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"/>
-        <style>
-          {`
-            .material-symbols-rounded {
-              font-variation-settings:
-              'FILL' 0,
-              'wght' 300,
-              'GRAD' 0,
-              'opsz' 24;
-              font-size: 20px;
-            }
-            .material-symbols-rounded.thin {
-              font-variation-settings:
-              'FILL' 0,
-              'wght' 150,
-              'GRAD' 0,
-              'opsz' 24;
-              font-size: 20px;
-            }
-            .material-symbols-rounded.filled {
-              font-variation-settings:
-              'FILL' 1,
-              'wght' 300,
-              'GRAD' 0,
-              'opsz' 24;
-              color: #ef4444;
-            }
-            .material-symbols-rounded.small {
-              font-size: 1rem;
-            }
-            .material-symbols-rounded.medium {
-              font-size: 3rem;
-            }
-            .material-symbols-rounded.large {
-              font-size: 7rem;
-            }
-            .hover-button:hover {
-              color: hsl(var(--muted-foreground));
-              background-color: hsl(var(--secondary));
-            }
-            .dark .hover-button:hover {
-              color: hsl(var(--muted-foreground));
-              background-color: hsl(var(--secondary));
-            }
-            .hover-underline:hover {
-              text-decoration: underline;
-            }
-          `}
-        </style>
       </head>
-      <body lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
         attribute="class"
         defaultTheme="system"
