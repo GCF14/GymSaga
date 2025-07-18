fcb57cc6a8c9ba168af3a7044c01e80efc664f8a
Christian Felipe <chrisfelipe525@gmail.com>
2025-07-19 04:10:30 +0800

    chore: set up comments directory for api directory

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkoutsModule } from './workouts/workouts.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [WorkoutsModule, PostsModule],
+  imports: [WorkoutsModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
