f7227128ac34f0f07810b879d3edcc8dc500f801
Christian Felipe <chrisfelipe525@gmail.com>
2025-07-19 04:19:25 +0800

    chore: set up auth directory for api directory

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { UploadModule } from './upload/upload.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [WorkoutsModule, PostsModule, CommentsModule, UploadModule, UsersModule],
+  imports: [WorkoutsModule, PostsModule, CommentsModule, UploadModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
