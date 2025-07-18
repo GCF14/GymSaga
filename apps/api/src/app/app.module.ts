0f73d49797e54dd02e72f189d199da447d565ed7
Christian Felipe <chrisfelipe525@gmail.com>
2025-07-19 04:39:22 +0800

    chore: set up mongoose in backend

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -7,9 +7,22 @@ import { CommentsModule } from './comments/comments.module';
 import { UploadModule } from './upload/upload.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WorkoutsModule, PostsModule, CommentsModule, UploadModule, UsersModule, AuthModule],
+  imports: [
+    WorkoutsModule, 
+    PostsModule, 
+    CommentsModule, 
+    UploadModule,
+    UsersModule, 
+    AuthModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
