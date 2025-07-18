5ca0a66a95bb3ca4402e4aace640b994a1f84dc9
Christian Felipe <chrisfelipe525@gmail.com>
2025-07-19 04:08:40 +0800

    chore: set up posts directory for api api directory

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkoutsModule } from './workouts/workouts.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [WorkoutsModule],
+  imports: [WorkoutsModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
