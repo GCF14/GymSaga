d065759f4efedfeb5764eb204e6d95ba1356fdbe
Christian Felipe <chrisfelipe525@gmail.com>
2025-07-29 01:02:41 +0800

    chore: update eslint and package.json

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,3 +1,16 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [...baseConfig];
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/jsx-sort-props': 'off',
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+];
